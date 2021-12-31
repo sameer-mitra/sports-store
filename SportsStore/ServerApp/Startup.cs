8499f61ab2860a89f6ef816e336eab00c4fc166c
Sameer Mitra <49610563+sameer-mitra@users.noreply.github.com>
2021-12-31 21:07:11 +1100

    SM 20211231 Added Database models.

--- SportsStore/ServerApp/Startup.cs
+++ SportsStore/ServerApp/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using ServerApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace ServerApp
 {
@@ -24,11 +27,22 @@ namespace ServerApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString =
+                Configuration["ConnectionStrings:DefaultConnection"];
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(connectionString));
+
             services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1",
+                    new OpenApiInfo { Title = "SportsStore API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
             if (env.IsDevelopment())
             {
@@ -54,6 +68,12 @@ namespace ServerApp
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
+            app.UseSwagger();
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "SportsStore API");
+            });
+
             app.UseSpa(spa =>
             {
                 string strategy = Configuration.GetValue<string>("DevTools:ConnectionStrategy");
@@ -64,7 +84,7 @@ namespace ServerApp
                     spa.Options.SourcePath = "../ClientApp";
                     spa.UseAngularCliServer("start");
                 }
-                
+                SeedData.SeedDatabase(services.GetRequiredService<DataContext>());
             });
         }
     }
