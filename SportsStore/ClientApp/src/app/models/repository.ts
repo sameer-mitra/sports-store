810d3909ac2b39d8274842cf8995ffbfe06c06e6
Sameer Mitra <49610563+sameer-mitra@users.noreply.github.com>
2022-01-03 11:38:10 +1100

    SM 20220103 Added support for filtering.

--- SportsStore/ClientApp/src/app/models/repository.ts
+++ SportsStore/ClientApp/src/app/models/repository.ts
@@ -1,6 +1,7 @@
 import { Product } from "./product.model";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Filter } from "./configClasses.repository";
 
 const productsUrl = "/api/products";
 
@@ -8,9 +9,13 @@ const productsUrl = "/api/products";
 export class Repository {
   product: Product;
   products: Product[];
+  filter: Filter = new Filter();
     
   constructor(private http: HttpClient) {
-    this.getProducts(true);
+    //this.getProducts(true);
+    this.filter.category = "soccer";
+    this.filter.related = true;
+    this.getProducts();
   }
 
   getProduct(id: number) {
@@ -23,10 +28,18 @@ export class Repository {
 
   getProducts(related = false) {
     console.log("Product Data Requested.");
-    this.http.get<Product[]>(`${productsUrl}?related=${related}`)
-      .subscribe(prods => {
-        this.products = prods;
-      });
+    //this.http.get<Product[]>(`${productsUrl}?related=${related}`)
+    //  .subscribe(prods => {
+    //    this.products = prods;
+    //  });
+    let url = `${productsUrl}?related=${this.filter.related}`;
+    if (this.filter.category) {
+      url += `&category=${this.filter.category}`;
+    }
+    if (this.filter.search) {
+      url += `&search=${this.filter.search}`;
+    }
+    this.http.get<Product[]>(url).subscribe(prods => this.products = prods);
   }
 
 }
